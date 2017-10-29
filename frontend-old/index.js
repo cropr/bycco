836c8c60a98fcf93ce0ad6e60cb3dea38df4490a
Ruben <ruben@decrop.net>
2017-10-29 16:32:25 +0100

    removing frontend-old

--- frontend-old/index.js
+++
@@ -1,7 +0,0 @@
-import "jquery";
-import "./js/menuscroll";
-
-import "../frontend/static/css/bycco.scss";
-import "../frontend/static/img/landing_big.jpeg";
-import "../frontend/static/img/landing_medium.jpeg";
-import "../frontend/static/img/landing_small.jpeg";
