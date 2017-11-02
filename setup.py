ba9c2ddb3cd8eabcd407830bca7fa8878ac07de6
Ruben <ruben@decrop.net>
2017-11-02 22:49:14 +0100

    v 0.1.3 de and fe translation, styling h?

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = []
 
 setup(
     name='bycco',
-    version='0.1.2',
+    version='0.1.3',
     description='Belgian Youtth Chess Championshops 2018',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
