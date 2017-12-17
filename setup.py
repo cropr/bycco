a5f0260071e5314fa7256d34130ea7f912008560
Ruben <ruben@decrop.net>
2017-12-17 18:50:30 +0100

    - subscription: made input fields column wide  - added 15 mmarch for non fide id players

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = []
 
 setup(
     name='bycco',
-    version='0.3.2',
+    version='0.3.4',
     description='Belgian Youtth Chess Championshops 2018',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
