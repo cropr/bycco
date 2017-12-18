af57d16e5fa5c616fae73a2fdbe654a5978fa22f
Ruben <ruben@decrop.net>
2017-12-18 15:26:33 +0100

    - solved translation bug base.html French  - removed natstatus debug info  - version 0.3.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = []
 
 setup(
     name='bycco',
-    version='0.3.5',
+    version='0.3.6',
     description='Belgian Youtth Chess Championshops 2018',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
