7c82148d91a8ac5048258e8b4e7a541ba3875709
Ruben <ruben@decrop.net>
2018-03-01 18:25:14 +0100

    updated confirmation date to 11 March

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = []
 
 setup(
     name='bycco',
-    version='0.4.0',
+    version='0.4.1',
     description='Belgian Youtth Chess Championshops 2018',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
