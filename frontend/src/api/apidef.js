c53ff0389f5f637bdcfa04ba3c1591d94bac1f1d
Ruben <ruben@decrop.net>
2018-03-04 15:19:35 +0100

    added publicity carousel made bridge working know bug: carousel is not turning in angular mode

---
+++ frontend/src/api/apidef.js
@@ -0,0 +1,83 @@
+export default {
+  // per api call define the required parameters per type
+  getAttendees: {
+    method: 'GET',
+    url: '/attendees',
+    query: ['ss', 'start', 'count', 'cat'],
+    required: [],
+  },
+  addAttendee: {
+    method: 'POST',
+    url: '/attendees',
+    body: ['attendee'],
+    required: ['attendee'],
+  },
+  getAttendee: {
+    method: 'GET',
+    url: '/attendees/{id}',
+    path: ['id'],
+    required: ['id'],
+  },
+  deleteAttendee: {
+    method: 'DELETE',
+    url: '/attendees/{id}',
+    path: ['id'],
+    required: ['id'],
+  },
+  updateAttendee: {
+    method: 'PUT',
+    url: '/attendees/{id}',
+    path: ['id'],
+    body: ['attendee'],
+    required: ['attendee', 'id'],
+  },
+  resendConfirmation: {
+    method: 'POST',
+    url: '/attendees/{id}/resend',
+    path: ['id'],
+    required: ['id'],
+  },
+  searchIdNational: {
+    method: 'GET',
+    url: '/belplayer/{idbel}',
+    path: ['idbel'],
+    required: ['idbel'],
+  },
+  searchIdFide: {
+    method: 'GET',
+    url: '/fideplayer/{idfide}',
+    path: ['idfide'],
+    required: ['idfide'],
+  },
+  confirmSubscription: {
+    method: 'POST',
+    path: ['idsub'],
+    url: '/subscriptions/{idsub}/confirm',
+    required: ['idsub']
+  },
+  createSubscription: {
+    method: 'POST',
+    url: '/subscriptions',
+    body: ['subscription'],
+    required: ['subscription']
+  },
+  updateSubscription: {
+    method: 'PUT',
+    url: '/subscriptions',
+    body: ['subscription'],
+    required: ['subscription']
+  },
+  getPhoto: {
+    method: 'GET',
+    url: '/subscriptions/{idsub}/photo',
+    path: ['idsub'],
+    required: ['idsub']
+  },
+  uploadPhoto: {
+    method: 'POST',
+    url: '/subscriptions/{idsub}/photo',
+    body: ['photo'],
+    path: ['idsub'],
+    required: ['photo', 'idsub']
+  },
+};
