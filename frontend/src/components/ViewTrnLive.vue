f2eb0684549ba2d794970691e0762330262086d7
Ruben <ruben@decrop.net>
2018-04-08 15:42:11 +0200

    live B14, G14, G16

--- frontend/src/components/ViewTrnLive.vue
+++ frontend/src/components/ViewTrnLive.vue
@@ -10,10 +10,12 @@
 </template>
 
 <script>
-const links = {
-  B10: "http://bycco.livechesscloud.com/view/#/8fc3e207-b149-4171-ab59-2ae8c882425c"
-}
 
+const links = {
+  B10: "http://bycco.livechesscloud.com/view/#/8fc3e207-b149-4171-ab59-2ae8c882425c",
+  B14: "http://bycco.livechesscloud.com/view/#/dd2b79f6-8afc-472e-a479-cf62f2e87beb",
+  G16: "http://bycco.livechesscloud.com/view/#/b7c7d269-10a4-4ee1-8b4f-2ad4eaad5e84",
+};
 
 export default {
   props: ['trn'],
