7a30bd166a98967c0f3ccf520fede6529714999f
Ruben Decrop <ruben@decrop.net>
2018-11-03 21:37:56 +0100

    vur.config for production

--- app/vue.config.js
+++ app/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     plugins: [
       new BundleTracker({
         indent: 2,
-        publicPath: process.env.NODE_ENV === 'production' ? 'static/' : 'http://localhost:8080/static/'
+        publicPath: process.env.NODE_ENV === 'production' ? '/static/' : 'http://localhost:8080/static/'
       })
     ]
   },
