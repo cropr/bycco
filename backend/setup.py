c317422be4c96de2bc7a106acf5425029d7119c7
Ruben Decrop <ruben@decrop.net>
2019-01-05 16:07:06 +0100

    version 0.8.6  - payment 35 euro ends 1st of March

--- backend/setup.py
+++ backend/setup.py
@@ -6,7 +6,7 @@ requires = []
 
 setup(
     name='bycco',
-    version='0.8.5',
+    version='0.8.6',
     description='BYC 2019',
     long_description='Belgian Youth Chess Cheampionships 2019',
     classifiers=[
