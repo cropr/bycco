342e0b71b02a16c4da5f69eb1569fe569689999c
Ruben Decrop <ruben@decrop.net>
2020-03-05 20:16:46 +0100

    added Cronos gold sponsor

--- frontend/src/components/AdCarousel.vue
+++ frontend/src/components/AdCarousel.vue
@@ -1,9 +1,19 @@
 <template>
+<div>
+  <v-card class="my-2 mx-auto" max-width="400">
+    <v-card-title>Gold Sponsor:</v-card-title>
+    <v-card-text>
+      <a href="http://www.cronos.be" target="_blank">
+      <img src='/static/img/frame_chronos.png'>
+      </a>
+    </v-card-text>
+  </v-card>
 <v-carousel :show-arrows="false" hide-delimiters :value="current" height="170"
             class="adcarousel mb-2" :interval="interval*1000" cycle continuous>
   <v-carousel-item v-for="item in items" :key="item.name" :src="item.src">
   </v-carousel-item>
 </v-carousel>
+</div>
 </template>
 
 <script>
